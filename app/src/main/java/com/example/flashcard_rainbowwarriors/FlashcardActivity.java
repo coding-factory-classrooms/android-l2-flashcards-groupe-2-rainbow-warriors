f8bba1f808b1614dfdc7fb186de228661e84ae31
Amine <a.amara86@gmail.com>
2021-02-19 09:36:23 +0100

    ajout images sons

--- app/src/main/java/com/example/flashcard_rainbowwarriors/FlashcardActivity.java
+++ app/src/main/java/com/example/flashcard_rainbowwarriors/FlashcardActivity.java
@@ -3,12 +3,13 @@ package com.example.flashcard_rainbowwarriors;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -44,9 +45,36 @@ public class FlashcardActivity extends AppCompatActivity {
         TextView questionTextView = findViewById(R.id.questionTextView);
         questionTextView.setText(flashcard.questionText);
 
-        // TODO look for sound or image resource when needed
         ImageView questionPictureView = findViewById(R.id.questionPictureView);
-        questionPictureView.setImageDrawable(getResources().getDrawable(R.drawable.home_gun));
+        Button playSoundButton = findViewById(R.id.playSoundButton);
+
+
+        // TODO look for sound or image resource when needed
+
+        if (flashcard.sourceType.equals("picture")){
+            playSoundButton.setVisibility(View.GONE);
+            int picId = getResources().getIdentifier(flashcard.sourceName, "drawable", getPackageName());
+            questionPictureView.setImageResource(picId);
+        }
+        if (flashcard.sourceType.equals("audio")){
+            questionPictureView.setVisibility(View.GONE);
+            int soundid = getResources().getIdentifier(flashcard.sourceName, "raw", getPackageName());
+            final MediaPlayer mp = MediaPlayer.create(FlashcardActivity.this, soundid);
+            playSoundButton.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    mp.start();
+                }
+            });
+        }
+
+
+
+        /*String icon="logo" + cnt;
+        int resID = getResources().getIdentifier(icon, "drawable",getPackageName());
+        imageViewExemple.setImageResource(resID);*/
+
+        //questionPictureView.setImageDrawable(getResources().getDrawable(R.drawable.home_gun));
+
         questionPictureView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -70,6 +98,7 @@ public class FlashcardActivity extends AppCompatActivity {
         findViewById(R.id.validateAnswerButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("testoulle", "toto");
                 RadioButton checkedButton = findViewById(radioGroup.getCheckedRadioButtonId());
                 Answer rightAnswer = null;
                 Answer selectedAnswer = null;
